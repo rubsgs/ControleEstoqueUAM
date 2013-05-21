2599b1a8c9dcbc1a808c080e3d3bc617ba4ecf0e
Erik Henrique <contato@erikhenrique.com.br>
2013-05-21 00:17:59 -0300

    Retirando teste do main.

--- Biblioteca/biblioteca/src/controleestoqueuam/AppLivraria.java
+++ Biblioteca/biblioteca/src/controleestoqueuam/AppLivraria.java
@@ -1,18 +1,12 @@
 package controleestoqueuam;
 
-import java.io.IOException;
-import jxl.read.biff.BiffException;
-import jxl.write.WriteException;
-
 /**
  *
  * Rubens Guarnieri, R.A.: 20303016
  * Erik Henrique, R.A.: 20301986
  */
 public class AppLivraria {
-    public static void main(String[] args) throws IOException, BiffException, WriteException {
-        jExcel excel;
-        excel = new jExcel();
+    public static void main(String[] args) {
         
         AppLivrariaController iniciar = new AppLivrariaController();
         
